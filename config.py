df6871e72790514d0854ef15b242f6162e42a38d
Doug McGeehan <djmvfb@mst.edu>
2018-09-15 10:09:05 -0500

    working demo ported from previous project

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import pathlib
 
 
 class defaults(object):
     '''Default configuration for application
     '''
-    input = '/tmp/python_script_skeleton.log'
+    # number of pixels to crop from the border of image
+    border_reduction = 50
+    intermediate_archival_directory = pathlib.Path('/tmp/scannedcoinsplitter/')
+    minimum_coin_area = 22179
+    cropped_output_directory = './results/cropped'
+    merged_output_directory = './results/merged'
