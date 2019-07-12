5c551f1c8d071202007bffd639d22f3a2e968820
Doug McGeehan <djmvfb@mst.edu>
2019-07-12 06:57:33 -0600

    parsing weight and name

--- create_inventory.py
+++ create_inventory.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pathlib
+import re
 import sys
 
 
@@ -9,7 +10,57 @@ def main(args):
     print(f'Iterating over scanned and named images in {scan_directory}')
 
     for scanned_image in scan_directory.glob('merged/*.png'):
-        print(f'\t{scanned_image}')
+        try:
+            ingot = Ingot(path=scanned_image)
+        except AttributeError:
+            continue
+        else:
+            print('.'*80)
+            print(ingot)
+            print(f'\tname   := {ingot.name}')
+            print(f'\tweight := {ingot.weight}')
+
+
+class WeightParser(object):
+    weight_regex = re.compile(r'.+(?P<value>\d+) (?P<unit>ozt|g).+')
+
+    weight_converter = {
+        'g': lambda v: v / 31.1,
+        'ozt': lambda v: v,
+    }
+
+    def __init__(self, filename: str):
+        parsed_weight = self.weight_regex.match(filename).groupdict()
+        self.weight_str = parsed_weight['value']
+        self.weight = float(self.weight_str)
+        if self.weight.is_integer():
+            self.weight = int(self.weight)
+
+        self.unit = parsed_weight['unit']
+
+    def to_ozt(self):
+        return self.weight_converter[self.unit](self.weight)
+
+    def __repr__(self):
+        return f'{self.weight_str} {self.unit}'
+
+
+class IngotNameParser(object):
+    def __init__(self, path: pathlib.Path, weight: WeightParser):
+        self.name = path.stem.replace(str(weight), '')
+
+    def __repr__(self):
+        return self.name
+
+
+class Ingot(object):
+    def __init__(self, path: pathlib.Path):
+        self.filename = path.name
+        self.weight = WeightParser(filename=self.filename)
+        self.name = IngotNameParser(path=path, weight=self.weight)
+
+    def __repr__(self):
+        return self.filename
 
 
 if __name__ == '__main__':
